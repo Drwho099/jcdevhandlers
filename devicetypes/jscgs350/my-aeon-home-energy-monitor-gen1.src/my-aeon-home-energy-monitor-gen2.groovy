9df7d1117b1d2ad4edadf189441ef0273c83dae1
Drwho099 <grhayek@gmail.com>
2017-07-25 16:02:19 -0700

    Update my-aeon-home-energy-monitor-gen2.groovy

--- devicetypes/jscgs350/my-aeon-home-energy-monitor-gen1.src/my-aeon-home-energy-monitor-gen2.groovy
+++ devicetypes/jscgs350/my-aeon-home-energy-monitor-gen1.src/my-aeon-home-energy-monitor-gen2.groovy
@@ -1,5 +1,5 @@
 /**
- *	Aeon HEM1
+ *	Aeon HEM2
  *
  *	Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *	in compliance with the License. You may obtain a copy of the License at:
@@ -58,7 +58,7 @@
  *
  */
 metadata {
-	definition (name: "My Aeon Home Energy Monitor Gen1", namespace: "jscgs350", author: "jscgs350")
+	definition (name: "My Aeon Home Energy Monitor Gen2 proto", namespace: "Drwho099", author: "jscgs350+")
 	{
 		capability "Energy Meter"
 		capability "Power Meter"
