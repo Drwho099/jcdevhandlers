c33aaca6776eb88b9b50dd6329a6270495c68de1
Drwho099 <grhayek@gmail.com>
2017-07-25 20:29:24 -0700

    Update and rename my-aeon-home-energy-monitor-gen2.groovy to my-aeon-home-energy-monitor-gen2-GRHTEST.groovy

--- devicetypes/jscgs350/my-aeon-home-energy-monitor-gen1.src/my-aeon-home-energy-monitor-gen2-GRHTEST.groovy
+++ devicetypes/jscgs350/my-aeon-home-energy-monitor-gen1.src/my-aeon-home-energy-monitor-gen2-GRHTEST.groovy
@@ -1,5 +1,5 @@
 /**
- *	Aeon HEM2
+ *	Aeon HEM2 YES!
  *
  *	Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *	in compliance with the License. You may obtain a copy of the License at:
